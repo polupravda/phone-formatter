bf2c44fb6e1f1b4a6a0ada1364e2c46fa731468a
Elena Volovicheva <elena.volovicheva@androidpit.de>
2021-01-18 08:50:44 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+function formatNumber(stripped) {
+  let areacode = stripped.slice(0, 3);
+  let first3 = stripped.slice(3, 6);
+  let last4 = stripped.slice(6);
+  let formatted = stripped;
+  if (stripped.length > 3) {
+    formatted = `(${areacode})`;
+    if (stripped.length < 7) {
+      formatted = `(${areacode})${first3}`;
+    }
+    if (stripped.length >= 7) {
+      formatted = `(${areacode})${first3}-${last4}`;
+    }
+    if (stripped.length > 10) {
+      formatted = stripped;
+    }
+  }
+  return formatted;
+}
 
 function App() {
+  const [rawPhone, setRawPhone] = useState("");
+
+  const handler = evt => {
+    const val = evt.currentTarget.value;
+    let phone = val;
+    let raw = phone.replace(/[^\d]/g, "");
+    let formatted = formatNumber(raw);
+    evt.currentTarget.value = formatted;
+    setRawPhone(`+1${raw}`);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" name="phone" onKeyUp={handler} />
+        <div className="rawContainer">
+          <p>{rawPhone}</p>
+        </div>
       </header>
     </div>
   );
